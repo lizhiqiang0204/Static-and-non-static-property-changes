a8f3de2953d3117208d991e0458182933c9e2582
unknown <18518092079@163.com>
2020-03-26 14:55:04 +0800

    static and non-static property change

---
+++ WpfApp1/ClassA.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.ComponentModel;
+
+namespace WpfApp1
+{
+    public class ClassA : INotifyPropertyChanged
+    {
+        private static string label = "静态字符";
+        public static string myLabel
+        {
+            get { return label; }
+            set
+            {
+                label = value;
+                StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(nameof(myLabel)));//异步更新静态属性
+            }
+        }
+        public static event EventHandler<PropertyChangedEventArgs> StaticPropertyChanged;//静态事件处理属性更改
+
+        private  string str = "非静态字符";
+        public  string myString
+        {
+            get { return str; }
+            set
+            {
+                str = value;
+                OnPropertyChanged("myString");//异步更新非静态属性
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;//非静态属性更改
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ClassA myClassA { get; set; } = new ClassA();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void ButtonA_Click(object sender, RoutedEventArgs e)
+        {
+            ClassA.myLabel = "更改绑定静态字符";
+            myClassA.myString = "更改绑定非静态字符串";
+        }
+    }
+}
